00fe997069e3469aaf5efd55bf23006f640ebd03
Skullcrusher619 <48494386+Skullcrusher619@users.noreply.github.com>
2026-06-07 08:47:46 -0500

    Update script.js
    updated the new embed url

--- script.js
+++ script.js
@@ -390,8 +390,8 @@ function openPlayer() {
   document.body.style.overflow = "hidden";
   
   let src = currentMedia.type === "series" 
-    ? `https://vidsrc.me/embed/tv?imdb=${currentMedia.imdbID}&season=${currentMedia.season}&episode=${currentMedia.episode}`
-    : `https://vidsrc.me/embed/movie?imdb=${currentMedia.imdbID}`;
+    ? `https://vsembed.ru/embed/tv?imdb=${currentMedia.imdbID}&season=${currentMedia.season}&episode=${currentMedia.episode}`
+    : `https://vsembed.ru/embed/movie?imdb=${currentMedia.imdbID}`;
     
   playerContainer.innerHTML = `<iframe src="${src}" allowfullscreen></iframe>`;
 }
